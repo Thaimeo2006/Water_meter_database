19d8c7bb320698aab724070317efd366fb0082c2
Thaimeo2006 <thaigioitoan@gmail.com>
2026-09-24 00:04:38 +0700

    Remove *.sql files

--- add_users_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id NVARCHAR(64) NOT NULL,
-    username NVARCHAR(32) NOT NULL,
-    password_hash NVARCHAR(255) NOT NULL,
-
-    CONSTRAINT PK_users PRIMARY KEY (id),
-    CONSTRAINT UQ_users_username UNIQUE (username)
-);
